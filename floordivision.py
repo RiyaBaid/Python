7131713ca13ce9df6a1487d3ec12b48adaf058d7
RiyaBaid <95839996+RiyaBaid@users.noreply.github.com>
2021-12-09 12:20:54 +0530

    Update floordivision.py

--- floordivision.py
+++ floordivision.py
@@ -15,7 +15,10 @@ print(x//y)
 # Find the general solution of x and y
 # x-> 2 y->9
 # Step1: Start
-# Step2: Get the serial number as n 
-# Step3: If n%2=0,then add the next odd number 
-# Step4: If n%2!=0,then subract the next odd number 
-# Step5:End
+# Step2: Initialise a variable named n
+# Step3:x=n+1
+# Step4:a=x+2
+# Step5:b=x-2
+# Step6: if x%2=0, then x+a
+# Step7: if x%2!=0, then x+b
+# Step8: Stop
